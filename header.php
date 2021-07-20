05f7eb6b87d445dd44be74eca7ce58fb79e47386
Adrien-Ray <a.raymond@codeur.online>
2021-07-20 15:37:27 +0200

    assets

--- header.php
+++ header.php
@@ -4,9 +4,10 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="shortcut icon" href="assets/logo/logo-shape-navbar.jpg" type="image/x-icon">
       <title>Shape Agency</title>
-      <link rel="stylesheet" href="styles/content.css">
-      <link rel="stylesheet" href="styles/includes.css">
+      <link rel="stylesheet" href="assets/styles/content.css">
+      <link rel="stylesheet" href="assets/styles/includes.css">
 </head>
 <body>
       
