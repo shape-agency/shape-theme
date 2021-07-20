cb44bcc5b0afbee9ab6ebb0c0be82728edc877fa
acs <a.bederiat@codeur.online>
2021-07-20 16:50:01 +0200

    front page avancement

--- front-page.php
+++ front-page.php
@@ -1,9 +1,10 @@
 <?php get_header();?>
-    <div>
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-1.png" alt="">
-            <div class="haut-de-page">
+    <div class="haut-de-page">
+        <img class="image-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/image-1.png" alt="">
+            <div class="article-haut">
                 <h1>SHAPE</h1>
                 <h3>Vos projets prennent forme.</h3>
+                <p>SHAPE est une jeune agence web basée à Dijon spécialisée dans la création de site vitrine et e-commerce pour les artisans, les indépendants et les TPE <br><br>Nous proposons une écoute personnalisée de vos besoins pour réaliser un site qui vous ressemble vraiment. <br><br> Récupérez une soluton clef en main avec un Wordpress sur-mesure et érez sans intermédiaire votre communication digitale</p>
             </div>
     </div>
 
