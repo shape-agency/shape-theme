3660ce65348a33044138cd3b26bc1dfeded20f3a
acs <a.bederiat@codeur.online>
2021-07-20 15:44:56 +0200

    function

--- front-page.php
+++ front-page.php
@@ -1,5 +1,11 @@
 <?php get_header();?>
-
+    <div>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-1.png" alt="">
+            <div class="haut-de-page">
+                <h1>SHAPE</h1>
+                <h3>Vos projets prennent forme.</h3>
+            </div>
+    </div>
 
 
 
---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+
+// ajout de fonctions au thème
+ add_theme_support( 'post-thumbnails' );
+add_theme_support( 'title-tag' );
+
+//ajout de style et des scripts
+function register_assets(){
+
+	wp_enqueue_script(
+		'wordpress',
+		'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js'
+		
+	);
+	
+	wp_enqueue_script(
+		'custom-scripts',
+		get_template_directory_uri().'/scripts/main.js',
+		 array(),
+		'1.0');
+		
+
+    wp_enqueue_style(
+		'shape-theme',
+		get_stylesheet_uri(),
+		 array(),
+		  '1.0');
+    
+	wp_enqueue_style(
+		'wordpress-css',
+		'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css'
+	);
+		  
+    if (is_front_page()){
+		wp_enqueue_style(
+		'shape-theme-custom-css',
+		get_template_directory_uri().'/style.css',
+		 array(),
+		  '1.0');
+}}
+add_action('wp_enqueue_scripts', 'register_assets');
