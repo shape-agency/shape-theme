298d777b0fc9d23889c148370f5585e6111b27d3
acs <a.bederiat@codeur.online>
2021-07-20 15:00:07 +0200

    front-page

--- front-page.php
+++ front-page.php
@@ -1,21 +1,6 @@
-<?php 
-include "includes/header.php";
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php get_header();?>
 
 
 
-<?php include "includes/footer.php" ?>
+
+<?php get_footer(); ?>
