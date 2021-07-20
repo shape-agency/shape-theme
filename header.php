a3142a0b7f320133878ae3faf34ac6930b8f4c14
Adrien-Ray <a.raymond@codeur.online>
2021-07-20 15:28:04 +0200

    header-title

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      link
+      <title>Shape Agency</title>
+      <link rel="stylesheet" href="styles/content.css">
+      <link rel="stylesheet" href="styles/includes.css">
 </head>
 <body>
       
