36ec3e4bb50163e67eeb4b3cc9f65c6ce05abcba
Adrien-Ray <a.raymond@codeur.online>
2021-07-22 15:37:03 +0200

    header3

--- header.php
+++ header.php
@@ -10,5 +10,12 @@
 </head>
 <body>
       <header>
-            <h1>shape</h1>
+            <div class="logo header__child">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" alt="">
+            </div>
+            <div class="nav header__child">
+                  <a href="" class="nav__child">L'agence</a>
+                  <a href="" class="nav__child">Nos services</a>
+                  <a href="" class="nav__child">Contact</a>
+            </div>
       </header>
