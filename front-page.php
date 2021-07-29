d48b587d6d20e44eb39b5ac25e6be2bb0329f873
sonia <s.rolland@codeur.online>
2021-07-29 16:54:06 +0200

    first

--- front-page.php
+++ front-page.php
@@ -7,7 +7,18 @@
                 <p>SHAPE est une jeune agence web basée à Dijon spécialisée dans la création de site vitrine et e-commerce pour les artisans, les indépendants et les TPE <br><br>Nous proposons une écoute personnalisée de vos besoins pour réaliser un site qui vous ressemble vraiment. <br><br> Récupérez une soluton clef en main avec un Wordpress sur-mesure et érez sans intermédiaire votre communication digitale</p>
             </div>
     </div>
-
+   
+    
+    <div class="milieu-de-page">
+        "">
+            <div class="article-milieu">
+                
+                <h3>FAITES-NOUS CONFIANCE</h3>
+                <p>Une question concernant notre fonctionnement?<br>Un devis détaillant les réponses que nous pouvons vous apportez vous seras transmis.<br>SHAPE ne fait pas de copier/coller:<br>Nous ne proposons que des stratégies web personnalisées à nos clients.</p>
+            </div>
+            <img class="image-2" src="<?php echo get_template_directory_uri(); ?>/assets/img/image-2.png" alt="">
+    </div>
+    </div>
 
 
 <?php get_footer(); ?>
