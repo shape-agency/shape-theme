35f298ad069ce63d051dbb843bc67a6271e80c5b
Adrien-Ray <a.raymond@codeur.online>
2021-07-29 16:12:24 +0200

    favicon

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="shortcut icon" href="assets/logo/logo-shape-navbar.jpg" type="image/x-icon">
+      <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" type="image/x-icon">
       <title>Shape Agency</title>
       <?php wp_head(); ?>
 </head>
