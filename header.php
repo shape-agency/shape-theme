c3d458c8d8be1fe9b91cff9742c7581ba0e772e6
Adrien-Ray <a.raymond@codeur.online>
2021-07-29 16:43:41 +0200

    burger with script hide block onclick

--- header.php
+++ header.php
@@ -13,12 +13,24 @@
             <div class="logo header__child">
                   <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" alt="">
             </div>
-            <div class="burger header__child">
+            <div class="burger header__child" id="burger">
                   <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/hamburger.png" alt="">
             </div>
-            <div class="nav header__child">
+            <div class="nav header__child" id="nav">
                   <a href="" class="nav__child">L'agence</a>
                   <a href="" class="nav__child">Nos services</a>
                   <a href="" class="nav__child">Contact</a>
             </div>
       </header>
+
+      <script>
+            document.getElementById('burger').addEventListener('click', hideShow);
+            function hideShow(){
+                  console.log('hello');
+                  if (document.getElementById('nav').style.display == 'none') {
+                        document.getElementById('nav').style.display = 'block';
+                  }else{
+                        document.getElementById('nav').style.display = 'none';
+                  }
+            }
+      </script>
