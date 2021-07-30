69267b127871e41dc2c74a03896bdb2c4139cc1d
sonia <s.rolland@codeur.online>
2021-07-30 15:13:19 +0200

    esthétique

--- front-page.php
+++ front-page.php
@@ -9,12 +9,11 @@
     </div>
    
     
-    <div class="milieu-de-page">
-        
-            <div class="article-milieu">
-                
-                <h3>FAITES-NOUS CONFIANCE</h3>
-                <p>Une question concernant notre fonctionnement?<br>Un devis détaillant les réponses que nous pouvons vous apportez vous seras transmis.<br>SHAPE ne fait pas de copier/coller:<br>Nous ne proposons que des stratégies web personnalisées à nos clients.</p>
+    <div class="centre">
+            <div class="art-centre">
+                <h2>FAITES-NOUS CONFIANCE</h2>   
+                <h3>Donnez forme à votre projet</h3>
+                <art>Une question concernant notre fonctionnement?<br>Un devis détaillant les réponses que nous pouvons vous apportez vous seras transmis.<br>SHAPE ne fait pas de copier/coller:<br>Nous ne proposons que des stratégies web personnalisées à nos clients.</art>
             </div>
             <img class="image-2" src="<?php echo get_template_directory_uri(); ?>/assets/img/image-2.png" alt="">
     </div>
