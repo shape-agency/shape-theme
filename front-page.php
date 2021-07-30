5f779d3590e1c8d4c838191dceb6218dd4d1b084
sonia <s.rolland@codeur.online>
2021-07-30 15:56:06 +0200

    maj theme

--- front-page.php
+++ front-page.php
@@ -7,11 +7,44 @@
                 <p>SHAPE est une jeune agence web basée à Dijon spécialisée dans la création de site vitrine et e-commerce pour les artisans, les indépendants et les TPE <br><br>Nous proposons une écoute personnalisée de vos besoins pour réaliser un site qui vous ressemble vraiment. <br><br> Récupérez une soluton clef en main avec un Wordpress sur-mesure et érez sans intermédiaire votre communication digitale</p>
             </div>
     </div>
-   
+    <div class="titre-haut-carte">
+        <div class="encadremenet-nos-services">
+        <h2 class="nos-services">NOS SERVICES</h2>
+        <div class="trait-nos-services"></div>
+        </div>
+    </div>
+    <div class="carte">
+        <div class="carte1">
+            <img class="img-carte" src="<?php echo get_template_directory_uri(); ?>/assets/img/plume.svg" alt="">
+            <h2 class="titre-carte">IDENTITÉ VISUELLE</h2>
+            <div class="trait-titre-carte"></div>
+            <p>Ensemble, bâtissons la communication visuelle de votre entreprise, elle a pour but de représenter la marque sur l'ensemble de ses supports de communication (logo, couleurs, typographies, iconographie...)</p>
+            </div>
+            
+        <div class="carte2">
+        <img class="img-carte" src="<?php echo get_template_directory_uri(); ?>/assets/img/code.svg" alt="">
+            <h2 class="titre-carte"> DÉVELOPPEMENT</h2>
+            <div class="trait-titre-carte"></div>
+        </div>
+        <div class="carte3">
+        <img class="img-carte" src="<?php echo get_template_directory_uri(); ?>/assets/img/fusee.svg" alt="">
+            <h2 class="titre-carte">WEBMARKETING</h2>
+            <div class="trait-titre-carte"></div>
+        </div>
+        
+    </div>
+    <div class="test"></div>
     
+
+    <div class="titre-centre">
+        <div class="encadremenet-faites-nous-confiance">
+        <h2 class="faites-nous-confiance">FAITES-NOUS CONFIANCE</h2>
+        <div class="trait-faites-nous-confiance"></div>
+        </div>
+    </div>
     <div class="centre">
             <div class="art-centre">
-                <h2>FAITES-NOUS CONFIANCE</h2>   
+                <h2></h2>   
                 <h3>Donnez forme à votre projet</h3>
                 <art>Une question concernant notre fonctionnement?<br>Un devis détaillant les réponses que nous pouvons vous apportez vous seras transmis.<br>SHAPE ne fait pas de copier/coller:<br>Nous ne proposons que des stratégies web personnalisées à nos clients.</art>
             </div>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="shortcut icon" href="assets/logo/logo-shape-navbar.jpg" type="image/x-icon">
+      <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" type="image/x-icon">
       <title>Shape Agency</title>
       <?php wp_head(); ?>
 </head>
@@ -13,9 +13,28 @@
             <div class="logo header__child">
                   <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" alt="">
             </div>
-            <div class="nav header__child">
-                  <a href="" class="nav__child">L'agence</a>
-                  <a href="" class="nav__child">Nos services</a>
-                  <a href="" class="nav__child">Contact</a>
+            <div class="burger header__child" id="burger">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/hamburger.png" alt="">
+            </div>
+            <div class="nav header__child" id="nav">
+                  <a href="#" class="nav__child">L'agence</a>
+                  <a href="#" class="nav__child">Nos services</a>
+                  <a href="#" class="nav__child">Contact</a>
             </div>
       </header>
+
+      <script>
+            let b = 0;
+            document.getElementById('burger').addEventListener('click', hideShow);
+            function hideShow(){
+                  console.log('hello');
+                  if (b == 0) {
+                        document.getElementById('nav').style.animation = 'menu 0.2s linear';
+                        document.getElementById('nav').style.animationFillMode = 'both';
+                        b = 1;
+                  }else{
+                        document.getElementById('nav').style.animation = 'menu2 0.2s linear';
+                        b = 0;
+                  }
+            }
+      </script>
