e9ef711038511377c4e22cdb8de32fbc9b9c04e0
Adrien-Ray <a.raymond@codeur.online>
2021-08-17 16:10:32 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 <?php
 newsletter_form();
 ?>
+<div class="initial_footer">
+      <div class="initial_footer__child">
+            Mentions légales
+      </div>
+      <div class="initial_footer__child">
+            2021 | Tous droits réservés SHAPE
+      </div>
+      <div class="initial_footer__child">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/logo/linkedin.png" alt="">
+      </div>
+</div>
 </body>
 </html>
