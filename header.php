e71534b8a32f90b7630afb445aaeafdf739d4c18
sonia <s.rolland@codeur.online>
2021-08-20 13:59:11 +0200

    correction

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" type="image/x-icon">
       <title>Shape Agency</title>
+      <link rel="stylesheet" href="style.css">
       <?php wp_head(); ?>
 </head>
 <body>
