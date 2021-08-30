326341845300624a5eda095efc98592513ccaff5
Adrien-Ray <a.raymond@codeur.online>
2021-08-30 16:37:10 +0200

    font ok

--- header.php
+++ header.php
@@ -6,6 +6,9 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/logo/logo-shape.png" type="image/x-icon">
       <title>Shape Agency</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Cuprum&family=Nunito+Sans&display=swap" rel="stylesheet"> 
       <?php wp_head(); ?>
 </head>
 <body>
