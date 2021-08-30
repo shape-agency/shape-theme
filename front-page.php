03e1eeff1553512034cde228f78f9b3e11854b54
Adrien-Ray <a.raymond@codeur.online>
2021-08-30 11:41:27 +0200

    maj

--- front-page.php
+++ front-page.php
@@ -228,7 +228,7 @@ while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
 
 
-    <div id="equipe" style="display:none">
+    <!-- <div id="equipe" style="display:none">
         <div class="container">
             <div id="equipe-h2-parent">
                 <h2 id="equipe-h2">NOTRE ÉQUIPE</h2>
@@ -255,7 +255,62 @@ while ( $loop->have_posts() ) : $loop->the_post(); ?>
              class="equipe_para"> Nous sommes une équipe de trois stagiaires de l'Access code school de Dijon.<br> <br> Nous avions pour objectif de fonder une agence web fictive, l'interface visiteur est un thème Wordpress personnalisé donc le contenu est administré par le back-office de Wordpress. <br> Nous avons choisi d'offrir une solution web adaptée aux besoins des artisans de Bourgogne-Franche-Comté. 
             </p>
         </div>
+    </div> -->
+
+
+
+
+
+
+
+
+
+<?php $loop = new WP_Query((array('post_type' => 'contact','order'=>'DSC', 'posts_per_page'=>1)));
+while ( $loop->have_posts() ) : $loop->the_post(); ?>
+<div id="equipe" style="display:none">
+    <div class="container">
+        <div id="equipe-h2-parent">
+            <h2 id="equipe-h2">NOTRE ÉQUIPE</h2>
+        </div>
+
+        <div class="equipe_card ">
+            <div class="equipe_card_membre">
+                <span>KARINA</span> <br> 
+                <img class="equipe_card_membre_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Avatar-karina.png" alt=""> <br> <br>
+                <span>Co-fondatrice <br>Web Designer </span>
+            </div>
+            <div class="equipe_card_membre">
+                <span>JEAN-BAPTISTE</span> <br> 
+                <img class="equipe_card_membre_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Avatar-JB.png" alt=""> <br> <br>
+                <span>Co-fondatrice <br>Web Designer </span>
+            </div>
+            <div class="equipe_card_membre">
+                <span>CHLOÉ</span> <br> 
+                <img class="equipe_card_membre_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Avatar-chloé.png" alt=""> <br> <br>
+                <span>Co-fondatrice <br>Web Designer </span>
+            </div>
+        </div>
+        <p 
+            class="equipe_para"> Nous sommes une équipe de trois stagiaires de l'Access code school de Dijon.<br> <br> Nous avions pour objectif de fonder une agence web fictive, l'interface visiteur est un thème Wordpress personnalisé donc le contenu est administré par le back-office de Wordpress. <br> Nous avons choisi d'offrir une solution web adaptée aux besoins des artisans de Bourgogne-Franche-Comté. 
+        </p>
     </div>
+</div>
+<?php endwhile; wp_reset_query(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <script>
         const ekip = document.getElementById('equipe');
         const button = document.getElementById('contact_para-2_bouton');
